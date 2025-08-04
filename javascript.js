86f3319539a9c3d0440a75ac6db326833978b672
Gabriel <27121228+gonzalezgabriel3@users.noreply.github.com>
2025-08-04 18:42:22 -0500

    Add button
    Added button and program is completely functional

--- javascript.js
+++ javascript.js
@@ -1,29 +1,39 @@
-const div = document.querySelector(".container");
+let div = document.querySelector(".container");
 div.setAttribute("style", "height: 600px; width: 600px;background-color: blue; display: flex; flex-direction:column; margin: auto;");
-const square = document.querySelector(".square");
+const btn = document.querySelector(".btn");
+const body = document.querySelector("body");
 
+newGrid(16);
+    
 
-let grid = function newGrid(columns){
+function newGrid(columns){   
     for (let i=0; i < columns; i++){
         
-       const squareColumn = document.createElement("div");
-        squareColumn.setAttribute("style", "display: flex; flex-direction: row; flex: 1; border: 1px dotted black;");
-        div.appendChild(squareColumn);
-         
+        const squareColumn = document.createElement("div");
+        squareColumn.setAttribute("style", "display: flex; flex-direction: row; flex: 1; ");        
+        div.appendChild(squareColumn);         
 
         for (let x=0; x<columns; x++){
-            const squareRow = document.createElement("div");
-            squareRow.setAttribute("class", "square");
+            const squareRow = document.createElement("div");            
             squareRow.setAttribute("style", "flex: 1; border: 1px dotted black;");
             squareRow.addEventListener("mouseover", ()=>{squareRow.style.backgroundColor = "black";})
             squareColumn.appendChild(squareRow); 
 
-        }
+        }     
         
-        
+    } 
+    
+    
+} 
+
+
+btn.addEventListener("click", (() => {
+    let userChoice;
+    do {
+        userChoice = prompt("Enter Desired Grid Size(Up to 100)");
     }
-    
-    
-    
-}
-grid(100); 
+    while (+userChoice < 1 || +userChoice > 100 || !(+userChoice));
+           
+        div.replaceChildren();       
+        newGrid(userChoice);          
+}))
