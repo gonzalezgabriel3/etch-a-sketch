88c85942abb84611a9bcd35ee19bd160c036a893
Gabriel <27121228+gonzalezgabriel3@users.noreply.github.com>
2025-08-04 21:33:02 -0500

    Add border to container div
    Added a border and changed the background color of the main div

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
-let div = document.querySelector(".container");
-div.setAttribute("style", "height: 600px; width: 600px;background-color: blue; display: flex; flex-direction:column; margin: auto;");
+const div = document.querySelector(".container");
+div.setAttribute("style", "height: 600px; width: 600px;background-color: #adb9ba; display: flex; flex-direction:column; margin: auto; border: 5px solid black;");
 const btn = document.querySelector(".btn");
-btn.setAttribute("style", "background-color: gold; padding: 8px; border-radius: 12px; font-size: 20px; display: block; margin: auto; margin-bottom: 18px; width: 200px;")
+btn.setAttribute("style", "background-color: gold; padding: 8px; border-radius: 12px; font-size: 20px;" 
+                +"display: block; margin: auto; margin-bottom: 18px; width: 200px;")
 
 newGrid(16);    
 
@@ -14,10 +15,16 @@ function newGrid(columns){
 
         for (let x=0; x<columns; x++){
             const squareRow = document.createElement("div");            
-            squareRow.setAttribute("style", "flex: 1; border: 1px dotted black;");
+            squareRow.setAttribute("style", "flex: 1; ");
             squareRow.addEventListener("mouseover", ()=>{
                 let randomColor = function (){return  Math.floor(Math.random() * 256); }
                 squareRow.style.backgroundColor = `rgb(${+randomColor()},${+randomColor()},${+randomColor()})`;
+                // squareRow.style.opacity = `0.5`;
+                let opac = 0;
+                // while (opac<101){
+                //     squareRow.setAttribute("style", `opacity: ${+opac}%` )
+                //     opac+=10;
+                // }
             });
             squareColumn.appendChild(squareRow); 
 
